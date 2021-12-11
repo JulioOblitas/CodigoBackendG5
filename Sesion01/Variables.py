7e31659af1784150f22af29067bf767976c5225d
JulioOblitas <89611439+JulioOblitas@users.noreply.github.com>
2021-12-10 20:20:10 -0500

    sesion01

---
+++ Sesion01/Variables.py
@@ -0,0 +1,40 @@
+#comentario
+numero = 10;
+numero2 = 10.5;
+
+
+valor = True
+nombre = """ jose 
+martin oblitas """
+
+print("hola")
+print(numero)
+print(nombre)
+print(type(nombre))
+print(type(valor))
+print(type(numero))
+print(type(numero2))
+
+#declarando arreglos o LIST 
+edades = [10,12,13,14, 'Eduardo', 14.50, False, [1,2] ]
+
+print(edades)
+
+#Json  (JavaScript Object Notation  ) | Diccionario
+curso = {
+    'nombre' : 'Backend',
+    'otros' : 'Otros' 
+}
+print(curso)
+
+print(curso['nombre']);
+
+
+
+
+
+
+
+
+
+
